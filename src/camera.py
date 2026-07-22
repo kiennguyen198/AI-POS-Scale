b48a7cc6fadcc4cbe6e9b74d18ed9b03765a705c
Nguyễn Trung Kiên <trungkienng1908@gmail.com>
2026-07-22 12:29:00 +0700

    Update camera

--- src/camera.py
+++ src/camera.py
@@ -1,7 +1,17 @@
 import cv2
-from config import CAMERA_SOURCE
+
+from config import CAMERA_SOURCE,FRAME_HEIGHT,FRAME_WIDTH
+
 def open_camera():
     cap=cv2.VideoCapture(CAMERA_SOURCE)
+    
+    if not cap.isOpened():
+        cap.release()
+        raise RuntimeError("Không thể mở camera")
+    
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,FRAME_WIDTH)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT)
+
     return cap
 
 def get_frame(cap):
