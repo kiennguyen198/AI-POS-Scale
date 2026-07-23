b31a3b69a5a66708086ec7e7636e8e8515574e7e
kminh1151 <kminh1151@gmail.com>
2026-07-23 11:11:42 +0700

    Update Sacale

--- src/config.py
+++ src/config.py
@@ -1,9 +1,20 @@
-# Lưu cấu hình để nhiều file khác cùng dùng như MODEL_PATH = "../models/best.pt" để sau này dễ sửa
-### Camera
+from pathlib import Path
+
+# root
+PROJECT_ROOT=Path(__file__).resolve().parent.parent
+
+# camera
 CAMERA_SOURCE=0
 FRAME_WIDTH=640
 FRAME_HEIGHT=480
 
-### AI
-MODEL_PATH='../models/best.pt'
+# AI
+MODEL_PATH=PROJECT_ROOT/'models'/'best.pt'
 CONFIDENCE_THRESHOLD=0.5
+
+# Database
+DATABASE_PATH=PROJECT_ROOT/'database'/'fruits.db'
+
+# Scale
+HX711_DT_PIN=5
+HX711_SCK_PIN=6
