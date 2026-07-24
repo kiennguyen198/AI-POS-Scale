49ddfc7d2a47d6ce290c46fb993f15a1c8da1966
Nguyễn Trung Kiên <trungkienng1908@gmail.com>
2026-07-24 09:33:25 +0700

    Add basic cart calculations

---
+++ src/cart.py
@@ -0,0 +1,19 @@
+def calculate_item_total(price_per_kg,weight_g):
+    item_total=round(price_per_kg*weight_g/1000)
+    return item_total
+
+def add_item(cart,fruit_name,weight_g,price_per_kg):
+    item_total=calculate_item_total(price_per_kg,weight_g)
+    item={  
+        "fruit_name":fruit_name,
+        "weight_g":weight_g,
+        "price_per_kg":price_per_kg,
+        "item_total":item_total
+    }
+    cart.append(item)
+
+def calculate_cart_total(cart):
+    cart_total=0
+    for item in cart:
+        cart_total+=item["item_total"]
+    return cart_total
